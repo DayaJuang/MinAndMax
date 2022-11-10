1e45f207d94e0992cb84000e51b11e809eec5863
daya <dayajuangmahaputra@gmail.com>
2022-11-10 18:24:27 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,41 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a scanner and min and max variable
+        Scanner scanner = new Scanner(System.in);
+        int min = 0;
+        int max = 0;
+        boolean isFirst = true;
+
+        while(true){
+            System.out.println("Enter a number : ");
+            boolean isValid = scanner.hasNextInt();
+            if(isValid){
+                int num = scanner.nextInt();
+                scanner.nextLine();
+                if(isFirst){
+                    min = num;
+                    max = num;
+                    isFirst = false;
+                }
+
+                if(num < min){
+                    min = num;
+                } else if(num > max){
+                    max = num;
+                }
+            }else{
+                System.out.println("Invalid Value");
+                break;
+            }
+        }
+
+        System.out.println("Min : " + min);
+        System.out.println("Max : " + max);
+
+        scanner.close();
     }
 }
